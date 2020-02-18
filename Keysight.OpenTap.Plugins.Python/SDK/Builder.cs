0a85a79edd9e96e6ece85ff6fbf99af2d48f074e
Rolf Madsen <rolf_madsen@keysight.com>
2020-02-18 16:25:16 +0100

    Added license headers.

--- Keysight.OpenTap.Plugins.Python/SDK/Builder.cs
+++ Keysight.OpenTap.Plugins.Python/SDK/Builder.cs
@@ -1,4 +1,10 @@
-﻿using System;
+﻿//  Copyright 2012-2019 Keysight Technologies
+//  Licensed under the Apache License, Version 2.0 (the "License");
+//  you may not use this file except in compliance with the License.
+//  You may obtain a copy of the License at
+//  http://www.apache.org/licenses/LICENSE-2.0
+
+using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
--- Keysight.OpenTap.Plugins.Python/SDK/GenericPythonWrapper.cs
+++ Keysight.OpenTap.Plugins.Python/SDK/GenericPythonWrapper.cs
@@ -1,4 +1,9 @@
-﻿using Keysight.OpenTap.Plugins.Python;
+﻿//  Copyright 2012-2019 Keysight Technologies
+//  Licensed under the Apache License, Version 2.0 (the "License");
+//  you may not use this file except in compliance with the License.
+//  You may obtain a copy of the License at
+//  http://www.apache.org/licenses/LICENSE-2.0
+using Keysight.OpenTap.Plugins.Python;
 using Python.Runtime;
 
 namespace Keysight.Plugins.Python
