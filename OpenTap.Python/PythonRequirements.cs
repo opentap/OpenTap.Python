cccbb8ccf13fbd25a6949aecf87dbdbb77c8fba1
romadsen-ks <rolf_madsen@keysight.com>
2022-07-14 13:45:08 +0200

    Fixed bug occuring when not able to load python.

--- OpenTap.Python/PythonRequirements.cs
+++ OpenTap.Python/PythonRequirements.cs
@@ -2,6 +2,7 @@ using OpenTap.Package;
 
 namespace OpenTap.Python;
 
+/// <summary> Specifies that a file is a python requirements file. </summary>
 public class PythonRequirements : ICustomPackageData
 {
     
