fe10fe5706c958dcccfa0ddff522ca0927dfe132
Rolf Madsen <rolf_madsen@keysight.com>
2022-08-04 17:46:53 +0200

    added --clear to search-path (#19)
    - added tap python search-path --clear.
    - simplified the code.

--- OpenTap.Python/PluginSearchPath.cs
+++ OpenTap.Python/PluginSearchPath.cs
@@ -1,7 +1,6 @@
 using System;
 using System.Collections.Generic;
 using System.IO;
-using System.Linq;
 namespace OpenTap.Python;
 public class PluginSearchPath : ValidatingObject
 {
@@ -28,7 +27,7 @@ public class PluginSearchPath : ValidatingObject
             return false;
         }, "This search path does not exist.", nameof(SearchPath));
 
-        Rules.Add(() => CheckDirectorySizeConstraint(), "The directory or its sub-directory(s) could not be accessed or they contain more than 100 files. (Maximum file count: 100)", nameof(SearchPath));
+        Rules.Add(CheckDirectorySizeConstraint, "The directory or its sub-directory(s) could not be accessed or they contain more than 100 files. (Maximum file count: 100)", nameof(SearchPath));
     }
 
     internal bool CheckDirectorySizeConstraint()
@@ -76,67 +75,5 @@ public class PluginSearchPath : ValidatingObject
         return true;
     }
 
-    TraceSource log = global::OpenTap.Log.CreateSource("Python");
-
-    public int ReadWritePluginSearchPath(string addSearchPath, string remSearchPath, bool getList)
-    {
-        var currentPluginSearchPath = PythonSettings.Current.SearchPathList;
-
-        if (getList)
-        {
-            if (currentPluginSearchPath.Count == 0)
-                log.Info("The additional search path list is empty.\n");
-            else
-            {
-                currentPluginSearchPath.ForEach(x =>
-                {
-                    string status = x.Enabled ? "Enabled" : "Disabled";
-                    log.Info($"Path: '{x.SearchPath}', Status: {status}\n");
-                });
-            }
-        }
-
-        if (!string.IsNullOrEmpty(addSearchPath.Trim()))
-        {
-            var searchPaths = new List<string>(addSearchPath.Split(';')).Select(x => x.Trim()).ToList();
-            searchPaths.RemoveAll(x => string.IsNullOrEmpty(x));
-            foreach (var path in searchPaths)
-            {
-                string fullSearchPath = Path.GetFullPath(path);
-                if (!Directory.Exists(fullSearchPath))
-                {
-                    log.Warning("Warning: The directory '{0}' does not exist.\n", fullSearchPath);
-                }
-                else if (currentPluginSearchPath.Exists(x => string.Compare(x.SearchPath, fullSearchPath) == 0))
-                    log.Warning("Warning: '{0}' exists in the additional search path list.\n", fullSearchPath);
-                else
-                {
-                    currentPluginSearchPath.Add(new PluginSearchPath() { SearchPath = fullSearchPath });
-                    log.Info("Added' {0}' to the additional search path list.\n", fullSearchPath);
-                }
-            }
-        }
-
-        if (!string.IsNullOrEmpty(remSearchPath.Trim()))
-        {
-            var searchPaths = new List<string>(remSearchPath.Split(';')).Select(x => x.Trim()).ToList();
-            searchPaths.RemoveAll(x => string.IsNullOrEmpty(x));
-            foreach (var searchPath in searchPaths)
-            {
-                string fullSearchPath = Path.GetFullPath(searchPath);
-                if (!Directory.Exists(fullSearchPath))
-                {
-                    log.Warning("Warning: The directory '{0}' does not exist.\n", fullSearchPath);
-                }
-                else if (currentPluginSearchPath.Remove(currentPluginSearchPath.Find(x => string.Compare(x.SearchPath, fullSearchPath) == 0)))
-                    log.Info("Removed '{0}' from the additional search path list.\n", fullSearchPath);
-                else
-                {
-                    log.Warning("Warning: The directory '{0}' is not in the additional search path list.\n", fullSearchPath);
-                }
-            }
-        }
-        PythonSettings.Current.Save();
-        return 0;
-    }
+    static readonly TraceSource log = Log.CreateSource("Python");
 }
