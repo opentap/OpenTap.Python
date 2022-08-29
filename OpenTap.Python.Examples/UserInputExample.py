4fd1b8d6016087f2e7d68d74518d29b7a6edbcc3
Rolf Madsen <rolf_madsen@keysight.com>
2022-08-29 13:10:07 +0200

    33 UserInputExample: Added (#34)
    * 33 UserInputExample: Added
    
    Added information on how to use UserInput.
    
    * Update UserInputExample.py

---
+++ OpenTap.Python.Examples/UserInputExample.py
@@ -0,0 +1,26 @@
+"""
+ A basic example of how to use User Input.
+"""
+from System import String, Object, Double
+import System.Threading
+import OpenTap
+from OpenTap import Display
+from opentap import *
+
+# Notice, this class inherits from System.Object(see line 4), a .NET class, not the default python object class.
+class BasicUserInput(Object):
+   Frequency = property(Double, 1.0).add_attribute(Display("Frequency", "The selected frequency."))
+   def __init__(self):
+      super().__init__()
+
+@attribute(OpenTap.Display("Basic User Input Step", "An example of asking for user input.", "Python Example"))
+class BasicUserInputStep(TestStep):
+   def __init__(self):
+      super().__init__()
+   def Run(self):
+      super().Run()
+      
+      obj = BasicUserInput()
+      # This should pop up a dialog asking the user to fill out the data in the object.
+      OpenTap.UserInput.Request(obj)
+      print(obj.Frequency)  
