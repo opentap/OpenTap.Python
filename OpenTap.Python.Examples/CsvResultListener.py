36e2065d734ffe057013ac503272e2cc8accc135
Rolf Madsen <rolf_madsen@keysight.com>
2023-11-14 10:51:57 +0100

    Added example of using MacroString to the CsvResultListener. (#119)

--- OpenTap.Python.Examples/CsvResultListener.py
+++ OpenTap.Python.Examples/CsvResultListener.py
@@ -6,6 +6,7 @@ import clr
 import math
 import opentap
 from opentap import *
+import os
 
 import System.IO
 from System.Text import StringBuilder
@@ -15,13 +16,18 @@ from OpenTap import Log, DisplayAttribute, Display, FilePathAttribute, FilePath
 
 @attribute(Display("CSV ResultListener", "An example of a ResultListener.", "Python Example"))
 class CsvPythonResultListener(PyResultListener):
-    FilePath = property(String, "MyFile.csv")\
+    # Use MacroString to get access to various macros for the file name.
+    # The <ResultName> macro is not supported in this example code as all the results 
+    # are written to the same file.
+    FilePath = property(OpenTap.MacroString, None)\
         .add_attribute(FilePath(FilePathAttribute.BehaviorChoice.Open, ".csv"))\
         .add_attribute(Display("File Path", "File path for results."))
     def __init__(self):
         super(CsvPythonResultListener, self).__init__() # The base class initializer must be invoked.
         self.sb = StringBuilder()
         self.Name = "PyCSV"
+        self.FilePath = OpenTap.MacroString()
+        self.FilePath.Text = "<Date>/test.csv"
 
     def OnTestPlanRunStart(self, planRun):
         """Called by TAP when the test plan starts."""
@@ -46,8 +52,16 @@ class CsvPythonResultListener(PyResultListener):
     def OnTestPlanRunCompleted(self, planRun, logStream):
         """Called by TAP when the test plan completes."""
         try:
+            # this code expands the macros used by FilePath.
+            fileName = self.FilePath.Expand(planRun)
             
-            System.IO.File.WriteAllText(self.FilePath, self.sb.ToString())
+            # Create directory if not exists
+            directory = os.path.dirname(fileName)
+            if not os.path.exists(directory):
+                os.makedirs(directory)
+
+            # Then write to the file.
+            System.IO.File.WriteAllText(fileName, self.sb.ToString())
         except Exception as e:
             self.log.Debug(e)
     
