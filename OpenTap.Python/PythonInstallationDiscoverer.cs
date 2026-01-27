ffa528a7fcdae3123fe34cef76ffd117d7983cc5
FrederikJA <fjaplv64@gmail.com>
2026-01-27 09:41:24 +0100

    Add python discovery path (#215)
    * Add python discovery path
    
    ---------
    
    Co-authored-by: Rolf Madsen <rolf_madsen@keysight.com>

--- OpenTap.Python/PythonInstallationDiscoverer.cs
+++ OpenTap.Python/PythonInstallationDiscoverer.cs
@@ -78,7 +78,8 @@ class PythonDiscoverer
             List<string> pys = new List<string>();
             foreach (var dir in Directory.GetDirectories(folderPath))
             {
-                if (Path.GetFileName(dir).Contains("Python"))
+                string fileName = Path.GetFileName(dir);
+                if (fileName.ToLower().Contains("python"))
                 {
                     pys.Add(dir);
                 }
@@ -104,8 +105,10 @@ class PythonDiscoverer
         var programFiles5 = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData),
             "Programs", "Python");
         var programFiles6 = Environment.GetFolderPath(Environment.SpecialFolder.ProgramFiles);
+        var programFiles7 = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData),
+            "Python");
 
-        return drives.Concat(new[] { programFiles, programFiles6, programFiles2, programFiles3, programFiles4, programFiles5 })
+        return drives.Concat(new[] { programFiles, programFiles2, programFiles3, programFiles4, programFiles5, programFiles6, programFiles7 })
             .SelectMany(GetPythonsInFolder).Distinct().ToArray();
     }
 
